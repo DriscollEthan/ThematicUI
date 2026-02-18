850f4ba5c6662bd247edefa2fedf6faa16c142bf
DriscollEthan <158096741+DriscollEthan@users.noreply.github.com>
2026-02-17 16:19:19 -0800

    Got Textblock taken care of. Working on adding extra themes before sharing Beta

--- Source/ThematicUI/Private/ThematicWidgets/ThematicUITextBlock.cpp
+++ Source/ThematicUI/Private/ThematicWidgets/ThematicUITextBlock.cpp
@@ -2,3 +2,76 @@
 
 
 #include "ThematicWidgets/ThematicUITextBlock.h"
+
+#include "Components/Border.h"
+#include "Components/SizeBox.h"
+#include "Components/TextBlock.h"
+
+void UThematicUITextBlock::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	
+	if (SizeBox)
+	{
+		SizeBox->SetWidthOverride(SizeBoxSize.X);
+		SizeBox->SetHeightOverride(SizeBoxSize.Y);
+	}
+	if (TextBlock)
+	{
+		TextBlock->SetText(Text);
+	}
+	
+	SetThemeNormal();																																		
+}
+
+void UThematicUITextBlock::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	SetThemeNormal();
+}
+
+void UThematicUITextBlock::SetThemeNormal()
+{
+	Super::SetThemeNormal();
+	
+	if (Border && TextBlock && WidgetTheme)
+	{
+		Border->SetBrush(WidgetTheme->NormalTheme.Image);
+		TextBlock->SetFont(WidgetTheme->NormalTheme.TextFont);
+	}
+	else
+	{
+		UE_LOGFMT(LogThematicUI, Error, "UThematicUITextBlock::SetThemeNormal : UThematicUITextBlock::Border == nullptr || UThematicUITextBlock::TextBlock == nullptr || UThematicUITextBlock::WidgetTheme == nullptr");
+	}
+}
+
+void UThematicUITextBlock::SetThemeHovered()
+{
+	Super::SetThemeHovered();
+	
+	if (Border && TextBlock && WidgetTheme)
+	{
+		Border->SetBrush(WidgetTheme->NormalTheme.Image);
+		TextBlock->SetFont(WidgetTheme->NormalTheme.TextFont);
+	}
+	else
+	{
+		UE_LOGFMT(LogThematicUI, Error, "UThematicUITextBlock::SetThemeHovered : UThematicUITextBlock::Border == nullptr || UThematicUITextBlock::TextBlock == nullptr || UThematicUITextBlock::WidgetTheme == nullptr");
+	}
+}
+
+void UThematicUITextBlock::SetThemePressed()
+{
+	Super::SetThemePressed();
+	
+	if (Border && TextBlock && WidgetTheme)
+	{
+		Border->SetBrush(WidgetTheme->NormalTheme.Image);
+		TextBlock->SetFont(WidgetTheme->NormalTheme.TextFont);
+	}
+	else
+	{
+		UE_LOGFMT(LogThematicUI, Error, "UThematicUITextBlock::SetThemePressed : UThematicUITextBlock::Border == nullptr || UThematicUITextBlock::TextBlock == nullptr || UThematicUITextBlock::WidgetTheme == nullptr");
+	}
+}
--- Source/ThematicUI/Public/ThematicWidgets/ThematicUITextBlock.h
+++ Source/ThematicUI/Public/ThematicWidgets/ThematicUITextBlock.h
@@ -13,4 +13,31 @@ UCLASS()
 class THEMATICUI_API UThematicUITextBlock : public UThematicUIInteractable
 {
 	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly, Category = "ThematicUI", meta=(AllowPrivateAccess, BindWidget))
+	TObjectPtr<class USizeBox> SizeBox;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "ThematicUI", meta=(AllowPrivateAccess, BindWidget))
+	TObjectPtr<class UBorder> Border;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "ThematicUI", meta=(AllowPrivateAccess, BindWidget))
+	TObjectPtr<class UTextBlock> TextBlock;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ThematicUI")
+	FVector2D SizeBoxSize = FVector2D(250.0f, 50.0f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ThematicUI ")
+	FText Text;
+	
+protected:
+	virtual void NativePreConstruct() override;
+	
+	virtual void NativeConstruct() override;
+	
+	virtual void SetThemeNormal() override;
+	
+	virtual void SetThemeHovered() override;
+	
+	virtual void SetThemePressed() override;
 };
